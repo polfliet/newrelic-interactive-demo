9a291e9b5b70b87778e8934a5c32862c318da653
Stijn Polfliet <spolfliet@newrelic.com>
2019-04-29 16:17:36 +0200

    Update script for EKS

--- loadgen.sh
+++ loadgen.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Specify the hostname, for example: ./loadgen.sh 3000"
+if [ "$#" -ne 3 ]; then
+    echo "Specify the hostname, port and sleep time, for example: ./loadgen.sh adc1ada716a8211e9a59d060b5087792-676580527.eu-west-1.elb.amazonaws.com 3000 0.2"
     exit
 fi
 
-HOST=192.168.99.100
-PORT=$1
+HOST=$1
+PORT=$2
+SLEEP=$3
   
 for i in {1..10000}
 do
         echo -n "$i - "
         /usr/bin/time curl http://$HOST:$PORT/action &
-        sleep 0.05
+        sleep $SLEEP
 done
